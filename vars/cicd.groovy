01f005b3b3040fc4f674e4ae7135e6dcd6ea62ac
E-Praveen <111327623+E-Praveen@users.noreply.github.com>
2023-03-15 19:52:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,16 @@ def newGit(repo)
 {
   git "${repo}"
 }
+def newMaven()
+{
+  mvn 'package' 
+}
+def newDeploy(jobname,ip,appname)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
+  
